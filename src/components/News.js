d0cde724cfc18aebb7649600bf91a4524a3fc5e6
leohon <leo.cl.hon@gmail.com>
2023-04-17 16:08:27 -0400

    Have first API working.

--- src/components/News.js
+++ src/components/News.js
@@ -1,20 +1,24 @@
 const News = () => {
   // Display the images
   function display(images) {
-    const gallery = document.querySelector(".newsGallery div");
-    const pElement = document.createElement("p");
-
-    gallery.innerHTML = " ";
-    pElement.innerText = images.data[0].title;
-    gallery.appendChild(pElement);
+    // const headline = images.data[0].title;
+    const imgContainer = document.querySelector(".imgContainer");
+    imgContainer.innerHTML = " ";
+    
+    //loop through for different images. 
+    // ***If img src = null???
+    for (let i = 0; i < images.data.length; i++) {
+      const imgElement = document.createElement("img");
+      imgElement.src = images.data[i].image;
+      imgContainer.appendChild(imgElement);
+    }
   }
 
   // Call the API
   async function callAPI(url) {
     const obj = await fetch(url);
     const images = await obj.json();
-    console.log(images.data[0].image);
-    console.log(images);
+    console.log(images)
     display(images);
   }
 
@@ -22,21 +26,27 @@ const News = () => {
   function endpoint() {
     const apiKey = '0fa169ec9d099a5466a62a98dc6fdfa2';
     const url = new URL("http://api.mediastack.com/v1/news");
+    const canada = "ca";
+    let date = new Date().toJSON().slice(0,10);
+    const sort = "popularity";
     
+    // Add dropdown for more variety? Doesn't really refresh well.
     url.search = new URLSearchParams({
       access_key: apiKey,
-      limit: 1
-      // date - today's date
+      countries: canada,
+      // date: date, //change to dates
+      limit: 3,
     })
+
     callAPI(url);
   }
 
   return (
     <section className="newsGallery">
       <button className="generate" id="generate" onClick={endpoint}>Generate</button>
-      <div>
-
-      </div>
+      <div className="imgContainer"></div>
+      <div className="imgContainer"></div>
+      <div className="imgContainer"></div>
     </section>
   )
 }
