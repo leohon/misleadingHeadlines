d4d7692fb6d057373c3477069efbc7c62cde614a
leohon <leo.cl.hon@gmail.com>
2023-04-19 23:08:28 -0400

    created form popup on image click and called second API

--- src/components/News.js
+++ src/components/News.js
@@ -1,32 +1,48 @@
+import { useState } from "react";
 import Form from "./Form";
 
 const News = () => {
-  //Form popup
-  function click() {
-    const form = document.querySelector(".form");
+  const [POS, setPOS] = useState([]);
+  const [tags, setTags] = useState([]);
+
+  function callAPITwo() {
+    // Grab the alt text and place sentence into an array of words
+    const test = document.querySelector(".newsGallery img")
+    const arr = test.alt.split(' ');
+    setPOS(arr);
     
-    // TODO: remove if statements, just need popup
-    if (form.style.display === "none") {
-      form.style.display = "block";
-    }
-    else {
-      form.style.display = "none";
-    }
+    // Second API
+    // https://npm.io/package/en-pos
+    const Tag = require("en-pos").Tag;
+    var tags = new Tag(arr).initial().smooth().tags;
+    setTags(tags);
+  }
+
+  function createForm() {
+    //Form popup
+    const form = document.querySelector(".form");
+    form.style.display = "block";
+
+    callAPITwo();
   }
 
   return (
     <section className="newsGallery">
       <div className="wrapper">
-        <div className="imgContainer" onClick={click}></div>
-        <div className="imgContainer" onClick={click}></div>
-        <div className="imgContainer" onClick={click}></div>
+        {/* May need to add parameter to createForm function */}
+        <div className="imgContainer" onClick={createForm}></div>
+        <div className="imgContainer" onClick={createForm}></div>
+        <div className="imgContainer" onClick={createForm}></div>
 
         {/* Temp placeholder for testing without wasting API calls */}
-        <div className="imgContainer" onClick={click}>
-          <img src="https://placekitten.com/200/200" alt="" />
+        <div className="imgContainer" onClick={createForm}>
+          <img src="https://placekitten.com/200/200" alt="Keep the big box on the table." />
         </div>
 
-        <Form />
+        <Form 
+          tags={tags}
+          POS={POS}
+        />
       </div>
     </section>
   )
