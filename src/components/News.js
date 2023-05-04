289f2a91cf12cd7747b9659bcf48647fb413063e
leohon <leo.cl.hon@gmail.com>
2023-05-04 18:29:04 -0400

    Hardcoded test to target selected choice.

--- src/components/News.js
+++ src/components/News.js
@@ -7,51 +7,53 @@ const News = function() {
   const [text, setText] = useState([]);
   const [tags, setTags] = useState([]);
 
-  const callAPITwo = function() {
+  const callAPITwo = function(target) {
     // Grab the alt text and place sentence into an array of words. Grab image source as well.
-    const image = document.querySelector(".newsGallery img")
-    const array = image.alt.split(' ');
-    const source = image.src;
-
+    const source = target.src;
+    const array = target.alt.split(' ');
+    
     // Second API
     // https://npm.io/package/en-pos
     const Tag = require("en-pos").Tag;
     var tags = new Tag(array).initial().smooth().tags;
-
+    
     setPhoto(source);
     setText(array);
     setTags(tags);
   }
+    
+// May: TODO - pass image properties by selected option only
 
-  const createForm = function() {
+  const createForm = function(event) {
     //Form popup
-    const form = document.querySelector("form");
+    const form = document.querySelector('form')
     form.style.display = "block";
-
-    callAPITwo();
+    
+    const target = event.target;
+    callAPITwo(target);
   }
 
   return (
     <section className="newsGallery">
       <div className="wrapper">
         {/* May need to add parameter to createForm function */}
-        <div className="gallery">
-          <div className="imgContainer" onClick={createForm}></div>
-          <div className="imgContainer" onClick={createForm}></div>
-          <div className="imgContainer" onClick={createForm}></div>
-        </div>
-        {/* Temp placeholder for testing without wasting API calls */}
         {/* <div className="gallery">
-          <div className="imgContainer" onClick={createForm}>
-            <img src="https://placekitten.com/200/200" alt="Keep the big box on the table." />
-          </div>
-          <div className="imgContainer" onClick={createForm}>
-            <img src="https://placekitten.com/200/200" alt="Keep the big box on the table." />
-          </div>
-          <div className="imgContainer" onClick={createForm}>
-            <img src="https://placekitten.com/200/200" alt="Keep the big box on the table." />
-          </div>
+          <div className="imgContainer" onClick={createForm}></div>
+          <div className="imgContainer" onClick={createForm}></div>
+          <div className="imgContainer" onClick={createForm}></div>
         </div> */}
+        {/* Temp placeholder for testing without wasting API calls */}
+        <div className="gallery">
+          <div className="imgContainer" id="imgOne" onClick={createForm}>
+            <img src="https://placekitten.com/200/200" alt="Keep the big box on the table." />
+          </div>
+          <div className="imgContainer two" onClick={createForm}>
+            <img src="https://placekitten.com/300/300" alt="Keep the big box on the table." />
+          </div>
+          <div className="imgContainer three" onClick={createForm}>
+            <img src="https://placekitten.com/400/400" alt="Keep the big box on the table." />
+          </div>
+        </div>
 
         <Form 
           tags={tags}
